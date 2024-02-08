5de62d333f93f3e399bb0083fc8e2cd83cc11291
Vikas jaiswal <125695126+Vikas-2023@users.noreply.github.com>
2024-02-08 23:21:18 +0530

    Initial commit

---
+++ script.js
@@ -0,0 +1,152 @@
+const searchbar=document.querySelector('.searchbar-container');
+const profilecontainer=document.querySelector('.profile-container')
+const root=document.documentElement.style;
+const url="https://api.github.com/users/";
+const noresults=document.querySelector('#no-results');
+const btnmode=document.querySelector('.btn-mode')
+const modetext=document.querySelector('#mode-text')
+const modeicon=document.querySelector('#mode-icon')
+const btnsubmit=document.querySelector("#sumbit")
+const input=document.querySelector("#input")
+const avatar=document.querySelector('#avatar')
+const userName=document.querySelector('#name')
+const user=document.querySelector('#user')
+const date=document.querySelector('#date')
+const months= ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"];
+const bio=document.querySelector('#bio')
+const repos=document.querySelector('#repos');
+const followers = document.querySelector('#followers');
+const following = document.querySelector('#following');
+const user_location = document.querySelector('#location');
+const page = document.querySelector('#page');
+const twitter = document.querySelector('#twitter');
+const company = document.querySelector('#company');
+let darkMode;
+ 
+btnsubmit.addEventListener("click",function(){
+  if(input.value!==""){
+    getUserData(url+input.value);
+  }
+})
+
+input.addEventListener("keydown",function(e){
+  if(e.key=="Enter"){
+    if(input.value!==""){
+      getUserData(url+input.value);
+    }
+    
+  }
+}
+);
+
+input.addEventListener("input",function(){
+  noresults.style.display="none";
+})
+
+btnmode.addEventListener("click",function(){
+  if(darkMode==false){
+    darkModeProperties();
+  }
+  else{
+    lightModeProperties();
+  }
+});
+
+//  async function getUserData(giturl){
+//   try{
+//     let response=await fetch(giturl);
+//     let data= await response.json();
+//     updateProfile(data);
+//   }
+//   catch(error){
+//     throw error;
+//   }
+ 
+//  }
+function getUserData(gitUrl) {
+  fetch(gitUrl)
+    .then((response) => response.json())
+    .then((data) => {
+      console.log(data);
+      updateProfile(data);
+    })
+    .catch((error) => {
+      throw error;
+    });
+}
+
+ function updateProfile(data){
+  if(data.message!=="Not Found"){
+    noresults.style.display="none";
+    function checkNull(param1, param2) {
+      if (param1 === "" || param1 === null) {
+        param2.style.opacity = 0.5;
+        param2.previousElementSibling.style.opacity = 0.5;
+        return false;
+      } else {
+        return true;
+      }
+    }
+    page.innerHTML=checkNull(data.blog,page) ? data.blog : "Not Available";
+    page.href = checkNull(data.blog, page) ? data.blog : "#";
+    company.innerHTML=checkNull(data.company,company) ? data.company : "Not Available";
+    twitter.innerHTML=checkNull(data.twitter_username,twitter) ? data.twitter_username : "Not Available";
+    twitter.href=checkNull(data.twitter_username,twitter) ? `https://twitter.com/${data.twitter_username}` : "#";
+    user_location.innerText = checkNull(data.location, user_location) ? data.location : "Not Available";
+    bio.innerHTML=data.bio===null?"This profile has no bio":`${data.bio}`;
+    user.innerHTML=`@${data.login}`;
+    user.href=`${data.html_url}`;
+    userName.innerHTML=`${data.name}`;
+    avatar.src=`${data.avatar_url}`;
+    followers.innerHTML=`${data.followers}`;
+    following.innerHTML=`${data.following}`
+    repos.innerHTML=`${data.public_repos}`;
+  }
+
+  else{
+    noresults.style.display="block";
+  }
+ }
+
+ function darkModeProperties(){
+  root.setProperty("--lm-bg", "#141D2F");
+  root.setProperty("--lm-bg-content", "#1E2A47");
+  root.setProperty("--lm-text", "white");
+  root.setProperty("--lm-text-alt", "white");
+  root.setProperty("--lm-shadow-xl", "rgba(70,88,109,0.15)");
+  modetext.innerText = "LIGHT";
+  modeicon.src = "/images/sun-icon.svg";
+  root.setProperty("--lm-icon-bg", "brightness(1000%)");
+  darkMode = true;
+  localStorage.setItem("dark-mode",true);
+ }
+
+ function lightModeProperties(){
+  root.setProperty("--lm-bg", "#F6F8FF");
+  root.setProperty("--lm-bg-content", "#FEFEFE");
+  root.setProperty("--lm-text", "#4B6A9B");
+  root.setProperty("--lm-text-alt", "#2B3442");
+  root.setProperty("--lm-shadow-xl", "rgba(70, 88, 109, 0.25)");
+  modetext.innerText = "DARK";
+  modeicon.src = "/images/moon-icon.svg";
+  root.setProperty("--lm-icon-bg", "brightness(100%)");
+  darkMode = false;
+  localStorage.setItem("dark-mode",false);
+ }
+ function init(){
+  // darkMode=false;
+  const value=localStorage.getItem("dark-mode");
+  if(value===null){
+    // localStorage.setItem("dark-mode", darkMode);
+    lightModeProperties();
+  }
+  else if(value=="true"){
+    darkModeProperties();
+  }
+  else if(value=="false"){
+    lightModeProperties();
+  }
+ getUserData(url+"Vikas-2023");
+ }
+
+ init();
